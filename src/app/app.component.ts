77e9fd734709c869fcb8c3826172a83743217f85
Ripon Ghosh <ripon.ghosh200@gmail.com>
2019-04-19 00:26:47 +0600

    sweetalert implement from offical documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'sweetalert2-angular';
+	title = 'sweetalert2-angular';
+	delete(){
+		Swal.fire({
+			title: 'Are you sure?',
+			text: "You won't be able to revert this!",
+			type: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Yes, delete it!'
+		}).then((result) => {
+			if (result.value) {
+				Swal.fire(
+					'Deleted!',
+					'Your file has been deleted.',
+					'success'
+					)
+			}
+		})
+	}
+
+	toast(){
+		const Toast = Swal.mixin({
+			toast: true,
+			position: 'top-end',
+			showConfirmButton: false,
+			timer: 3000
+		});
+
+		Toast.fire({
+			type: 'success',
+			title: 'Signed in successfully'
+		})
+	}
+
+	dialog(){
+		Swal.fire({
+			position: 'top-end',
+			type: 'success',
+			title: 'Your work has been saved',
+			showConfirmButton: false,
+			timer: 1500
+		})
+	}
+
+	dialogMiddle(){
+		Swal.fire({
+			// position: 'top-end',
+			type: 'success',
+			title: 'Your work has been saved',
+			showConfirmButton: false,
+			timer: 1500
+		})
+	}
 }
